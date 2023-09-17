bac9aa4cd5a4bde16348dde60fcb0fee47aeefb4
Wutieyin <136192696+Wutieyin@users.noreply.github.com>
2023-09-17 06:07:03 +0100

    W01 assignment

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,48 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+
+        Console.WriteLine();
+        Console.Write("What was your grade percentage in CSE 210? ");
+        string yourScore = Console.ReadLine();
+        int x = int.Parse(yourScore);
+
+        if (x >= 90)
+        {
+            Console.WriteLine("Wow! You got an A");
+        }
+
+        else if (x < 90 && x >= 80)
+        {
+            Console.WriteLine("You got a B.");
+        }
+
+        else if (x < 80 && x >= 70)
+        {
+            Console.WriteLine("You got a C.");
+        }
+
+        else if (x < 70 && x >= 60)
+        {
+            Console.WriteLine("You got a D.");
+        }
+        
+        else
+        {
+            Console.WriteLine("You got a F.");
+
+        }
+
+        if (x >= 70)
+        {
+            Console.WriteLine("Congratulation! You passed the course.");
+        }
+
+        else
+        {
+            Console.WriteLine("Not yet. Work harder and you will pass it next semester.");
+        }
+        Console.WriteLine();
+
     }
 }
