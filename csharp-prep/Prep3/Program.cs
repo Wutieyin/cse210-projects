2d1c7a3df0949c7d3409b841b27adf245746d7be
Wutieyin <136192696+Wutieyin@users.noreply.github.com>
2023-09-22 06:29:42 +0100

    Assignment

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.WriteLine();
+
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        int guess = -1;
+
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+            
+            if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+
+            else if (guess < magicNumber)
+            {
+                Console.WriteLine("Higer");
+            }
+
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }   
+        Console.WriteLine();
     }
+
+    
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,50 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+
+        List<int> numbers = new List<int>();
+
+        int yourNumber = -1;
+        while (yourNumber != 0)
+        {
+            Console.Write("Enter a number (0 to quit): ");
+            
+            string yourResponse = Console.ReadLine();
+            yourNumber = int.Parse(yourResponse);
+        
+
+             if (yourNumber != 0)
+            {
+                numbers.Add(yourNumber);
+            }
+        }
+
+        // Part 1: Compute the sum
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+
+        Console.WriteLine($"The sum is: {sum}");
+
+        // Part 2: Find the average
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        // Part 3: Find the max
+        
+        int max = numbers[0];
+
+        foreach (int number in numbers)
+        {
+            if (max < number)
+            {
+                // if the max is less than the number, the number becomes the new max!
+                max = number;
+            }
+        }
+
+        Console.WriteLine($"The max is: {max}");
     }
 }
